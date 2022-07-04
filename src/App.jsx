fc83b6c7a6f751ade6c10e2c365b1a672a78fafb
israel natanov <israeln753951@gmail.com>
2022-07-04 14:01:38 +0300

    firs commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+import Index from './screens/home'
+import Home from './screens/home'
+import Library from './screens/library'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      < Index />
+      
+    </div>
+  )
+}
+
+export default App
